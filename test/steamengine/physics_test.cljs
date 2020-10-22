92d0be9445e2273d570e5afa0cdec249eb38d3a4
Alessandro Solbiati <lessandro@fb.com>
2020-10-22 03:21:21 +0100

    [physics] diffusion, neighbours working 4 lines, unbounded

--- test/steamengine/physics_test.cljs
+++ test/steamengine/physics_test.cljs
@@ -5,21 +5,14 @@
     ))
 
 (deftest test-diffusion-neighbours-no-boundaries
-  (is (= [[4] [6]] (p/nd-neighbours [5] 1))) 
-  (is (= [[5 4] [5 6]] (p/nd-neighbours [5 5] 2))) 
-  (is (= [[5 5 4] [5 5 6]] (p/nd-neighbours [5 5 5] 3))) 
-  (is (= [[4 5] [6 5]] (p/d-plus-1 [[4] [6]] 5 )))
-  (is (= [[4 5 5] [6 5 5]] (p/d-plus-1 [[4 5] [6 5]] 5 )))
-  (is (= [[5 6] [5 4] [4 5] [6 5]] (p/neighbours [5 5])))
-  (is (= [[3 6] [3 4] [2 5] [4 5]] (p/neighbours [3 5])))
-  (is (= [[5 5 6] [5 5 4] [5 6 5] [5 4 5] [4 5 5] [6 5 5]] (p/neighbours [5 5 5])))
-  (is (= [[5 6] [5 4] [4 5] [6 5]] (p/neighbours [5 5])))
-  (is (= (* 4 2) (count (p/neighbours [5 5 5 5]))))
+  (is (= (set [[5 6] [5 4] [4 5] [6 5]]) (set (p/neighbours [5 5]))))
+  (is (= (set [[2 6] [2 4] [1 5] [3 5]]) (set (p/neighbours [2 5]))))
+  (is (= (set [[2 6 8] [2 4 8] [1 5 8] [3 5 8] [2 5 7] [2 5 9]]) (set (p/neighbours [2 5 8]))))
 )
 
 (deftest test-diffusion-neighbours-boundaries
-  ;;(is (= [[1]] (p/nd-neighbours 0 1))) 
-  ;;(is (= [[19]] (p/nd-neighbours 20 1 20))) 
-  ;;(is (= [ 1] (p/neighbours [0 5])))
-  )
+  (is (= (set [[0 6] [0 4] [1 5]]) (set (p/neighbours [0 5]))))
+  ;;(is (= (set [[2 6] [2 4] [1 5] [3 5]]) (set (p/neighbours [2 5]))))
+  ;;(is (= (set [[2 6 8] [2 4 8] [1 5 8] [3 5 8] [2 5 7] [2 5 9]]) (set (p/neighbours [2 5 8]))))
+)
 
